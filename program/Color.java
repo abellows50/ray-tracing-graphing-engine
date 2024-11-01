c97467d22190a2c0fe382b0767dfe431bcb4d5f4
abellows50 <abellows50@stuy.edu>
2024-11-01 16:01:07 -0400

    updated color

--- program/Color.java
+++ program/Color.java
@@ -1,26 +1,31 @@
 public class Color{
     private int r,g,b;
-    
-    public Color(int r, int g, int b){
+    private double intensity;
+
+    public Color(int r, int g, int b, double intensity){
         this.r = r;
         this.g = g;
         this.b = b;
+        this.intensity = intensity;
     }
 
     public void add (Color c){
         return new Color((this.r + c.getR())%255, 
                          (this.g + c.getG())%255, 
-                         (this.b + c.getB())%255);
+                         (this.b + c.getB())%255,
+                         this.intensity + c.getIntensity());
     }
 
     //toString
-    public String toString(){return "(" + this.r + ", " + this.g + ", " + this.b + ")";}
+    public String toString(){return "(" + this.r + ", " + this.g + ", " + this.b + "," + this.intensity")";}
 
     //Setter and Getters
     public void setR(int r){this.r = r;}
     public void setG(int g){this.g = g;}
     public void setB(int b){this.b = b;}
+    public void setIntensity(double i){this.intensity = i;}
     public int getR(){return this.r;}
     public int getG(){return this.g;}
     public int getB(){return this.b;}
+    public double getIntensity(){return this.intensity;}
 }
